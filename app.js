03e52a23f8041af78e1f3c473488e6a5907cc26c
Arsalan <marsalan750@gmail.com>
2024-09-06 09:38:06 +0500

    Files for assignment 12

---
+++ app.js
@@ -0,0 +1,208 @@
+            //Class Practice //
+
+// alert("Please click OK to return to home page")
+
+// var firstName= "Muhammad";
+// var lastName= "Arsalan";
+
+// console.log(firstName,lastName)
+// console.log("My name is "+firstName,lastName)
+
+
+        // Chapter 1 (Alerts) //
+// Alert 1 //
+// alert("Error! Please enter a valid password.");
+
+// Alert 2 //
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Alert 3 //
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// Alert 4 //
+// alert("Hello... I can run JS through my web browser's console")
+// console.log("Hello... I can run JS through my web browser's console");
+
+
+
+        // Chapter 2 (Variables for Strings) //
+
+    // 1.
+        // var username;
+
+    // 2.
+        // var myName;
+        // myName = "Muhammad Arsalan";
+
+    // 3.
+        // a)
+            // var message;
+        // b)
+            // message = "Hello World!";
+        // c)
+            // alert(message);
+
+    // 4.
+        // var name;
+        // name="Arsalan";
+        // var age;
+        // age="15 years old";
+        // var course
+        // course="Web & App development";
+
+        // alert(name);
+        // alert(age);
+        // alert(course);
+
+    // 5.
+        // alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+    // 6.
+        // var email="johndoe@gmail.com";
+        // alert("My email address is "+email);
+
+    // 7.
+    // var book="A smarter way to learn JavaScript";
+    // alert("I am trying to learn from the Book "+ book);
+
+    // 8.
+    // document.write("<h1>Yah! I can write in HTML content through Javascript</h1>");
+
+    // 9.
+    // var character="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+    // alert(character);
+
+    
+        // Chapter 3 (Variables for Numbers) //
+
+    // 1.
+    // var age= 15;
+    // alert("I am " + age + " years old");
+
+    // 2.
+    // var visits = 5; 
+    // document.write("You have visited this site " + visits + " times.");
+
+    // 3.
+    // var birthYear = 1998; 
+    // document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is " + typeof(birthYear));
+
+    // 4.
+    // var visitorName = "Arsalan";
+    // var productTitle = "T-shirt";
+    // var quantity = 5; 
+    // document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+            
+    
+            // Chapter 4 (Variable Names: Legal & Illegal)
+
+    // 1.
+    // var variable1, variable2, variable3;
+
+    // 2.
+    // var myVariable;
+    // var $price;
+    // var _name;
+    // var age;
+    // var total_amount;
+
+    // 3.
+    // var 1stVariable; 
+    // var my-variable; 
+    // var @name;
+    // var var; 
+    // var true; 
+
+    // 4.
+    // document.write("<h1>Rules for naming JS variables</h1>");
+    // document.write("Variable names can only contain letters, numbers, $, and _. For example: $my_1stVariable<br>");
+    // document.write("Variables must begin with a letter, $, or _. For example: $name, _name, or name<br>");
+    // document.write("Variable names are case sensitive<br>");
+    // document.write("Variable names should not be JS reserved keywords");
+
+    
+    // Assignment for Home (Chapter1-3.docx) //
+
+    // Chapter 1 (Alerts)
+
+    // 1.
+    // alert("First Name: Muhammad");
+    // alert("Last Name: Arsalan");
+    // alert("Email: Arsalan@gmail.com");
+    // alert("Phone Number: 0900-78601");
+    // alert("Password: @myPassword@");
+
+    // 2.
+    // Incorrect:  alert "You're learning JavaScript!";
+    // Correct: alert("You're learning JavaScript!");
+
+    // 3.
+    // alert("Welcome to JavaScript programming!");
+
+    // Chapter 2 (Variables for Strings)
+
+    // 1.
+    // var myFirstVariable;
+
+    // 2.
+    // var message;
+    // message = "Hello, World!";
+
+    // 3.
+    // var teamName = "The Web Team";
+    // alert(teamName);
+
+    // 4.
+    // var bestMan = "ABCD";
+    // bestMan = "XYZ";
+
+
+    // Chapter 3 (Variables for Numbers)
+
+    // 1.
+    // var caseQty;
+
+    // 2.
+    // caseQty = 144;
+
+    // 3.
+    // var num = 9;
+
+    // 4.
+    // var sum;
+    // sum = 5 + 10;
+
+    // 5.
+    // var merchTotal = 100;
+    // var shippingCharge = 10;
+    // var orderTotal = merchTotal + shippingCharge;
+   
+    // 6.
+    // var runs = 50;
+    // runs = runs + 20; 
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
